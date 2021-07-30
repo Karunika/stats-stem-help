f795f52dbb934eb6466ecc4c06e29d15d3d7487a
nopeless <38830903+nopeless@users.noreply.github.com>
2021-07-31 03:07:25 +0900

    add port parser

--- server/index.js
+++ server/index.js
@@ -1,10 +1,23 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
-const apiPort = 3000
 const Db = require('./db')
 const db = new Db()
 const cors = require('cors')
+const yargs = require('yargs')
+
+const argv = yargs
+    // accept port number as argument
+    .option('port', {
+        alias: 'p',
+        default: 3000,
+        describe: 'Port number',
+        type: 'number'
+    }).argv
+
+console.log(argv)
+
+const apiPort = argv.port
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors())
