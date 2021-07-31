404df32d5c65656c809ee6de8d7377346336fbdd
nopeless <38830903+nopeless@users.noreply.github.com>
2021-07-31 17:17:02 +0900

    softcode api route

--- server/index.js
+++ server/index.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const app = express()
 const Db = require('./db')
 const db = new Db()
 const cors = require('cors')
 const yargs = require('yargs')
+
+const expressApp = express();
+const apiRouter = express.Router();
+
+require('dotenv').config()
 
 const argv = yargs
     // accept port number as argument
@@ -19,16 +23,25 @@ console.log(argv)
 
 const apiPort = argv.port
 
-app.use(bodyParser.urlencoded({ extended: true }))
+expressApp.use(bodyParser.urlencoded({ extended: true }))
 // do not use cors on production (delt on nginx side)
 if (process.env.NODE_ENV !== 'production')
-    app.use(cors())
-app.use(bodyParser.json())
+    expressApp.use(cors())
 
-app.get('/:userId', async (req, res)=>{
+expressApp.use(bodyParser.json())
+
+apiRouter.get('/:userId', async (req, res)=>{
    let answer = await db.getUser(req.params.userId)
    res.send(answer)
    return answer
 })
 
-app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`))
+if (process.env.API) {
+    expressApp.use(process.env.API, apiRouter)
+    console.log(`API listening on '${process.env.API}'`)
+} else {
+    expressApp.use('/', apiRouter)
+    console.log(`API listening on '/' ROOT`)
+}
+
+expressApp.listen(apiPort, () => console.log(`Server running on port ${apiPort}`))
